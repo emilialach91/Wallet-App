dfa0deb57f623c9cf0709db476971b57271c676b
Emilia <emilia.lach.91@gmail.com>
2021-03-03 16:22:43 +0000

    add slider img

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { DatePipe } from '@angular/common'
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [HttpClientModule, BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, DatePipe],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router, NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,12 +9,66 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  data: any;
+  balance: any;
+  transactions: any;
+  suggestions: any;
+
+  constructor(public http: HttpClient, private router: Router) {}
+
+  ionViewWillEnter() {
+    
+      this.http
+        .get('https://7f9065f949ced63703b704d9dd14f7a8.m.pipedream.net')
+        .subscribe((data)=>{
+          this.data = data
+
+          this.balance = data["wallet"].balance
+          this.transactions = data["transactions"]
+          this.suggestions = data["pay_with_etn"].suggestions
+
+          console.log('data', this.data)
+          console.log('suggestions', this.suggestions)
+
+
+        })
+    }
+
+    // openTransactionsWithState() {
+    //   let navigationExtras: NavigationExtras = {
+    //     state: {
+    //       transactions: this.transactions
+    //     }
+    //   };
+    //   this.router.navigate(['transactions'], navigationExtras);
+    // }
+
+    // openTransactionWithState(id, topUp) {
+
+    //   if (topUp === true) {
+    //     let navigationExtras: NavigationExtras = {
+    //       state: {
+    //         transaction: this.transactions.find(transaction => transaction.id === id)
+    //       }
+    //     };
+    //     this.router.navigate(['top-up'], navigationExtras);
+    //   } else {
+    //     let navigationExtras: NavigationExtras = {
+    //       state: {
+    //         transaction: this.transactions.find(transaction => transaction.id === id)
+    //       }
+    //     };
+    //     this.router.navigate(['outgoing-transfer'], navigationExtras);
+    //   }
+
+    //   console.log(id, topUp)
+    //   console.log(this.transactions.find(transaction => transaction.id === id))
+    // }
 
   option = {
     slidesPerView: 3.5,
-    spaceBetween: 5,
     loop: false,
+    spaceBetween: 10,
     freeMode: true,
   }
 }
